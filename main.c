7bb4493fc5b23e2fbecc6e5cdbbc8747078de8ec
lsy0313 <03lsy13@naver.com>
2023-11-07 22:27:04 +0900

    실습5

--- main.c
+++ main.c
@@ -1,32 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5
+#define ROWS 3
+#define COLS 3
 
 static int all_files;
 extern void sub();
 
-void square_array(int a[], int size)
-{
- int i;
- for (i = 0; i < size; i++)
-     a[i] = a[i] * a[i];
+void addMatrix(int A[ROWS][COLS], int B[ROWS][COLS], int C[ROWS][COLS]) {
+     int i, j;
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
 }
 
-void print_array(int a[], int size)
-{
- int i;
- for (i = 0; i < size; i++)
-     printf("%3d", a[i]);
-     printf("\n");
+void printMatrix(int A[ROWS][COLS]) {
+     int i, j;
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            printf("%d ", A[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int main(int argc, char *argv[])
 {
-  int list[SIZE] = {1, 2, 3, 4};
-  
-  print_array(list, SIZE);
-  square_array(list, SIZE);
-  print_array(list, SIZE);
+  int A[ROWS][COLS] = { 
+ {2, 3, 0},
+ {8, 9, 1},
+ {7, 0, 5} };
+ int B[ROWS][COLS] = { 
+ {1, 0, 0},
+ {0, 1, 0},
+ {0, 0, 1} };
+ int C[ROWS][COLS]; 
+ 
+ addMatrix(A, B, C);
+ printMatrix(C);
 
   system("PAUSE");	
   return 0;
