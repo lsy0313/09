ad74d4d2473c09eca9dc2184dd66f3461d4ec189
lsy0313 <03lsy13@naver.com>
2023-11-07 22:18:12 +0900

    실습4

--- main.c
+++ main.c
@@ -5,22 +5,31 @@
 static int all_files;
 extern void sub();
 
+void square_array(int a[], int size)
+{
+ int i;
+ for (i = 0; i < size; i++)
+     a[i] = a[i] * a[i];
+}
+
+void print_array(int a[], int size)
+{
+ int i;
+ for (i = 0; i < size; i++)
+     printf("%3d", a[i]);
+     printf("\n");
+}
+
 int main(int argc, char *argv[])
 {
-  int i; 
-  int flag = 0;
-  int a[SIZE] = {1, 2, 3, 4, 5};
-  int b[SIZE] = {1, 2, 3, 4, 5};
+  int list[SIZE] = {1, 2, 3, 4};
   
-  for (i = 0; i < SIZE; i++){
-      if (a[i] != b[i]){
-         printf("array a and b are not the same \n");
-         flag = 1;
-      }
-  }
-      if (flag == 0)
-         printf("array a and b are the same \n");
+  print_array(list, SIZE);
+  square_array(list, SIZE);
+  print_array(list, SIZE);
 
   system("PAUSE");	
   return 0;
 }
+
+
